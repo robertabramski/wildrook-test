64c51279143c6edb6e0cbc9051e5ee231f77c4a8
robertabramski <iam@robertabramski.com>
2018-07-16 13:45:29 -1000

    Add expanding graph bar rollover.

--- src/app/components/severity-graph/severity-graph.component.ts
+++ src/app/components/severity-graph/severity-graph.component.ts
@@ -8,16 +8,35 @@ import { NAV_PADDING } from '../../app.constants';
   templateUrl: './severity-graph.component.html',
   styleUrls: ['./severity-graph.component.less'],
   inputs: ['scrs'],
-  outputs: ['severityInfo']
+  outputs: ['severityInfo', 'graphWidths']
 })
 export class SeverityGraphComponent implements OnInit {
   scrs:Scr[] = null;
-  severityInfo:object = null;
+  severityInfo:object = {};
+  graphWidths:object = {};
+
+  private ROLLOVER_PERCENT_INCREASE = 15;
 
   constructor(private scrollToService:ScrollToService) { }
 
   ngOnInit() {
-    this.severityInfo = this.getSeverityInfo(this.scrs);
+    this.createGraphData(this.scrs);
+  }
+
+  revertGraphRollover() {
+    for(let severity in this.severityInfo) {
+      let percentage = this.severityInfo[severity].percentage;
+      this.graphWidths[severity] = percentage;
+    }
+  }
+
+  expandGraph(key) {
+    for(let severity in this.graphWidths) {
+      if(severity === key) {
+        let origPercentage = this.severityInfo[key].percentage;
+        this.graphWidths[key] = origPercentage + this.ROLLOVER_PERCENT_INCREASE;
+      }
+    }
   }
 
   scrollToSeverity(severity) {
@@ -31,24 +50,26 @@ export class SeverityGraphComponent implements OnInit {
     this.scrollToService.scrollTo(config);
   }
 
-  private getSeverityInfo(scrs):object {
+  private createGraphData(scrs) {
     /*
     Create object for severity specifics. Derive percentage and
     count data from the returned server data.
-    */
-    let severityInfo = {};
 
+    Rather than creating another loop for graph width data, handle
+    in this function as general data parsing.
+    */
     scrs.forEach(scr => {
-      if(!(scr.severity in severityInfo)) {
-        severityInfo[scr.severity] = {count: 1, level: scr.level};
+      if(!(scr.severity in this.severityInfo)) {
+        this.severityInfo[scr.severity] = {count: 1, level: scr.level};
       } else {
-        let info = severityInfo[scr.severity];
+        let info = this.severityInfo[scr.severity];
 
         info.count++;
         info.percentage = Math.round((info.count / scrs.length) * 100);
+
+        // Add graph data for rollovers.
+        this.graphWidths[scr.severity] = info.percentage;
       }
     });
-
-    return severityInfo;
   }
 }
